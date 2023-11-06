6dca36af874d40f30f08a5cd88b4531118719b44
Iceman (The Ace One) <95144833+ayush1017@users.noreply.github.com>
2023-11-06 22:17:06 +0530

    Add files via upload

---
+++ Ashmovies.js
@@ -0,0 +1,118 @@
+function clik() {
+    window.open("https://filmyworlds.cloud/download-loki-2021-season-1-480p-720p/", "_self");
+}
+
+
+
+$(document).ready(function () {
+
+
+    $('#2').click(function () {
+        window.open("https://filmyworlds.cloud/?s=Alice+in+border+land+", "_self");
+
+    });
+
+
+    $('#3').click(function () {
+        window.open("https://filmyworlds.cloud/?s=6+underground", "_self");
+    });
+
+    $('#4').click(function () {
+        window.open("https://filmyworlds.cloud/?s=a+million+miles", "_self");
+    });
+
+    $('#5').click(function () {
+        window.open("https://filmyworlds.cloud/download-the-silent-sea-season-1-480p-720p/", "_self");
+    });
+
+
+
+
+    $('#6').click(function () {
+        window.open("https://filmyworlds.cloud/download-jawan-2023-hdcamrip-hindi-full-movie-480p-720p-1080p/", "_self");
+    });
+
+    $('#7').click(function () {
+        window.open("https://filmyworlds.cloud/download-jailer-2023-v3-hq-hdcam-hindi-dubbed-org-line-full-movie-480p-720p-1080p/", "_self");
+    });
+
+    $('#8').click(function () {
+        window.open("https://filmyworlds.cloud/download-don-2-2011-hindi-full-movie-web-dl-480p-720p-1080p/", "_self");
+    });
+
+    $('#9').click(function () {
+        window.open("https://filmyworlds.cloud/download-pathaan-2023-hindi-full-movie-web-dl-480p-720p-1080p-2160p-4k/", "_self");
+    });
+
+    $('#10').click(function () {
+        window.open("https://filmyworlds.cloud/download-fast-x-fast-and-furious-x-2023-imax-hindi-org-480p-720p-1080p/", "_self");
+    });
+
+
+
+    $('#11').click(function () {
+        window.open("https://filmyworlds.cloud/download-ben-10-secret-of-the-omnitrix-2007-dual-audio-hindi-english-480p-720p/", "_self");
+    });
+    $('#12').click(function () {
+        window.open("://filmyworlds.cloud/httpsdownload-ben-10-race-against-time-2007-dual-audio-hindi-english-480p-720p/", "_self");
+    });
+    $('#13').click(function () {
+        window.open("https://filmyworlds.cloud/?s=power+rangers", "_self");
+    });
+
+    $('#14').click(function () {
+        window.open("https://filmyworlds.cloud/download-zootopia-2016-dual-audio-hindi-english-480p-720p-1080p/", "_self");
+    });
+
+    $('#15').click(function () {
+        window.open("https://filmyworlds.cloud/download-soul-2020-480p-720p/", "_self");
+    });
+
+
+    $('#searchButton').click(function () {
+        var searchText = $('#searchBox').val();
+
+        if (searchText == "loki") {
+            window.open("https://filmyworlds.cloud/download-loki-2021-season-1-480p-720p/", "_self");
+        }
+        if (searchText == "Alice in Borderland") {
+            window.open("https://filmyworlds.cloud/?s=Alice+in+border+land+", "_self");
+        }
+        if (searchText == "6 Underground") {
+            window.open("https://filmyworlds.cloud/?s=6+underground", "_self");
+        }
+        if (searchText == "the silent sea") {
+            window.open("https://filmyworlds.cloud/download-the-silent-sea-season-1-480p-720p/", "_self");
+        }
+        if (searchText == "jawan") {
+            window.open("https://filmyworlds.cloud/download-jawan-2023-hdcamrip-hindi-full-movie-480p-720p-1080p/", "_self");
+        }
+    });
+
+
+    $('#16').click(function () {
+        window.open("https://filmyworlds.solutions/english-movies/", "_self");
+    });
+    $('#17').click(function () {
+        window.open("https://filmyworlds.solutions/hindi-movies-download/", "_self");
+    });
+    $('#18').click(function () {
+        window.open("https://filmyworlds.solutions/animation/", "_self");
+    });
+
+    $('#19').click(function () {
+        window.open("https://filmyworlds.solutions/comedy/", "_self");
+    });
+
+    $('#20').click(function () {
+        window.open("https://filmyworlds.cloud/download-soul-2020-480p-720p/", "_self");
+    });
+
+    $('#log').click(function () {
+        window.open("http://127.0.0.1:5500/Ash.html", "_self");
+    });
+
+
+
+});
+
